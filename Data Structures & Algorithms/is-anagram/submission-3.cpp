0ce67dd5b4ef400bb91c1a4dc674599f48391521
Sai Kishore <60418281+SaiKishore-hash@users.noreply.github.com>
2026-05-10 11:16:08 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> tally;
+        if (s.size()!=t.size()) return false;
+
+        for (int i=0; i<s.size(); i++){
+            tally[s[i]]++;
+            tally[t[i]]--;
+        }
+
+        for (auto pair : tally){
+            if (pair.second!=0) return false;
+        }
+        return true;
+    }
+};
