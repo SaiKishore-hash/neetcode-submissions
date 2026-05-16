e3f77232dd5b5326f5010f4993c2e9500851a308
Sai Kishore <60418281+SaiKishore-hash@users.noreply.github.com>
2026-05-16 15:14:31 +0530

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> folders;
+
+        for (auto s : strs){
+            string root = s;
+            sort(root.begin(),root.end());
+            folders[root].push_back(s);
+        }
+
+        vector<vector<string>> result;
+
+        for (auto x : folders){
+            result.push_back(x.second);
+        }
+        return result;
+    }
+};
