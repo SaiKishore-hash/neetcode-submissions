aae1f0663a86e0818a9c1aa09660bbaa3b61e774
Sai Kishore <60418281+SaiKishore-hash@users.noreply.github.com>
2026-05-16 15:09:23 +0530

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> notebook;
+
+        for (int i=0; i<nums.size(); i ++){
+            int complement = target - nums[i];
+            if (notebook.count(complement)) return {notebook[complement],i};
+            notebook[nums[i]]=i;
+        }
+        return {};
+    }
+};
